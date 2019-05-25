bac43869aef0a052b3b9d1c705bdd0b6cb84d9bf
Ciprian Sabou <csabou@epharmasolutions.com>
2019-05-25 22:22:53 +0200

    read environment

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -4,15 +4,22 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration configurationSection;
+
+        public HomeController(IConfiguration configurationSection)
+        {
+            this.configurationSection = configurationSection;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View("Index", this.configurationSection.GetValue<string>("Environment"));
         }
 
         public IActionResult Privacy()
